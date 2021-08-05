a0b8a2c6bcdafbbf05f786204fd2f494331c7482
ShogoSonoda <sonoponsyougo@gmail.com>
2021-08-05 17:19:25 +0900

    [新規機能追加]axiosとproxyをインストール
    API通信をするため

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   server: {
     port: 8080
   },
-  
+
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
@@ -45,7 +45,15 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
   ],
+
+  proxy: {
+    '/api': {
+      target: 'http://localhost:3000/'
+    }
+  }
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
