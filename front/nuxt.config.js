24b6ebdf05dd1e4011209fa6686c0a7f966e5e8c
ShogoSonoda <sonoponsyougo@gmail.com>
2021-08-05 11:56:35 +0900

    [機能修正]nuxtのportを8080に変更
    3000だとrails側とバッティングしてしまうため

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -1,6 +1,10 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  server: {
+    port: 8080
+  },
+  
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
