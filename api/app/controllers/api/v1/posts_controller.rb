8498be3e3a8da6af9e61a826ee854e0106bc5789
ShogoSonoda <sonoponsyougo@gmail.com>
2021-08-23 20:54:54 +0900

    [add]createアクション実装(Rails)

--- api/app/controllers/api/v1/posts_controller.rb
+++ api/app/controllers/api/v1/posts_controller.rb
@@ -4,6 +4,15 @@ module Api
       def index
         @posts = Post.all
       end
+
+      def create
+        post = Post.new(content: params[:post][:content])
+        if post.save
+          render json: '作成に成功しました', status: 200
+        else
+          render json: '作成に失敗しました', status: 500
+        end
+      end
     end
   end
 end
