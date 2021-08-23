6097e3977552799a3c395d77c5693a75a4b71ad4
ShogoSonoda <sonoponsyougo@gmail.com>
2021-08-23 19:14:52 +0900

    [add]axios通信
    ページがマウントされたタイミングで、api/v1/postsにGETリクエストを送るメソッドを実行

--- front/pages/posts/index.vue
+++ front/pages/posts/index.vue
@@ -1,12 +1,40 @@
 <template>
-  <div>
-    この中にHTMLを記述する
+  <div class="container py-5">
+    <b-card
+      v-for="post in posts"
+      :key="post.id"
+    >
+    <b-card-text>
+      {{ post.content }}
+    </b-card-text>
+    </b-card>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data: () => {
+    return {
+      posts: [],
+    }
+  },
+
+  mounted() {
+    this.fetchContents()
+  },
+
+  methods: {
+    fetchContents() {
+      const url = "/api/v1/posts"
+      this.$axios.get(url)
+        .then((res) => {
+          console.log(res)
+        })
+        .catch(() => {
+
+        })
+    },
+  }
 }
 </script>
 
